c62e849e0f224ed55e0b553d9b7d73b68a39c45a
wsy <43888221@qq.com>
2017-08-09 19:24:13 +0800

    @Value注解使用

--- spring-boot-quickStart/src/main/java/com/ss/quickStart/web/UserController.java
+++ spring-boot-quickStart/src/main/java/com/ss/quickStart/web/UserController.java
@@ -3,8 +3,11 @@ package com.ss.quickStart.web;
 import com.ss.quickStart.conf.PropertiesValues;
 import com.ss.quickStart.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.MessageFormat;
 
 /**
  * Created by wsy on 2017/8/8.
@@ -14,11 +17,16 @@ public class UserController {
     @Autowired
     private PropertiesValues propertiesValues;
 
+    //取配置文件中author.name值，若没有则赋值为ss
+    @Value("${author.name:ss}")
+    private String name;
+
     @RequestMapping("getUser.do")
     public User getUser(){
         User user = new User();
-        user.setName(propertiesValues.getUserName()+"-SS");
+        user.setName(propertiesValues.getUserName());
         user.setSex(1);
+        System.out.println(MessageFormat.format("name=>{0}",name));
         return user;
     }
 }
